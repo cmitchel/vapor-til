df8ca7463d107285b4f6ff4d2bc17189642847c1
Mitch <chris.mitchell@me.com>
2018-10-26 16:16:23 +0100

    test2

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,6 @@
 import Vapor
 
-/// Register your application's routes here.
+/// Register your application's routes here. test2
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
